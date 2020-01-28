0ad7df9c7f4fa96ccab9249b94c00573b2ed4eaf
cajaks2 <cajaks2@gmail.com>
2020-01-28 00:33:45 -0800

    little clean up add pass through device name

--- awair.py
+++ awair.py
@@ -1,5 +1,4 @@
 from prometheus_client import start_http_server, Gauge, Summary
-import random
 import time
 import argparse
 from pyawair.auth import *
@@ -7,7 +6,6 @@ from pyawair.data import *
 
 
 
-# Create a metric to track time spent and requests made.
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 AWAIR_SCORE = Gauge("prom_awair_score", "Awair score of device")
 AWAIR_TEMP = Gauge("prom_awair_temp", "Awair temp of device")
@@ -16,15 +14,12 @@ AWAIR_CO2 = Gauge("prom_awair_co2", "Awair co2 level of device")
 AWAIR_VOC = Gauge("prom_awair_voc", "Awair voc of device")
 AWAIR_PM25 = Gauge("prom_awair_pm25", "Awair pm25 of device")
 
-# Decorate function with metric.
 @REQUEST_TIME.time()
-def retrieve_data(auth=""):
+def retrieve_data(auth="",device_name="Bedroom"):
     data = ""
     try:
-        data = (get_current_air_data(auth, device_name='Bedroom'))
-        #prom_output  = []
+        data = (get_current_air_data(auth, device_name=device_name))
         for device in data:
-            #prom_output.append("prom_awair_score {}".format(device['score']))
             AWAIR_SCORE.set(device['score'])
             for sensor in device['sensors']:
                 if sensor['comp'] == 'temp':
@@ -44,7 +39,6 @@ def retrieve_data(auth=""):
         print(data)
 
 if __name__ == '__main__':
-    # Start up the server to expose the metrics.
     parser = argparse.ArgumentParser()
     parser.add_argument('--token', help='auth-token from Awair',required=True)
     parser.add_argument('--interval',type=int,default=300, help='interval time watch for quota limits, 300s for hobby level')
