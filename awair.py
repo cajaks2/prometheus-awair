d96eae2fcc30c911b6ec87c4819977bc5c0887a3
cajaks2 <cajaks2@gmail.com>
2020-01-29 01:06:16 -0800

    prom metric name switch add label

--- awair.py
+++ awair.py
@@ -9,13 +9,14 @@ import traceback
 
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 RESPONSE_CODE = Counter('awair_reponse_code', 'HTTP Response Codes', ['http_code'])
-AWAIR_SCORE = Gauge("prom_awair_score", "Awair score of device")
-AWAIR_TEMP = Gauge("prom_awair_temp", "Awair temp of device")
-AWAIR_HUMID = Gauge("prom_awair_humid", "Awair humidity of device")
-AWAIR_CO2 = Gauge("prom_awair_co2", "Awair co2 level of device")
-AWAIR_VOC = Gauge("prom_awair_voc", "Awair voc of device")
-AWAIR_PM25 = Gauge("prom_awair_pm25", "Awair pm25 of device")
 FAILURE_COUNT = Counter('awair_failure_count', 'AWAIR API FAILURES', ['method'])
+#initialize as none to avoid being 0 
+AWAIR_SCORE = None
+AWAIR_TEMP = None
+AWAIR_HUMID = None
+AWAIR_CO2 = None
+AWAIR_VOC = None
+AWAIR_PM25 = None
 
 
 @REQUEST_TIME.time()
@@ -23,19 +24,26 @@ def retrieve_data(auth="",device_name="Bedroom"):
     data = ""
     try:
         data = (get_current_air_data(auth, device_name=device_name))
+        if AWAIR_SCORE is None:
+            AWAIR_SCORE = Gauge("awair_device_score", "Awair score of device",['device'])
+            AWAIR_TEMP = Gauge("awair_device_temp", "Awair temp of device",['device'])
+            AWAIR_HUMID = Gauge("awair_device_humid", "Awair humidity of device",['device'])
+            AWAIR_CO2 = Gauge("awair_device_co2", "Awair co2 level of device",['device'])
+            AWAIR_VOC = Gauge("awair_device_voc", "Awair voc of device",['device'])
+            AWAIR_PM25 = Gauge("awair_device_pm25", "Awair pm25 of device",['device'])
         for device in data:
             AWAIR_SCORE.set(device['score'])
             for sensor in device['sensors']:
                 if sensor['comp'] == 'temp':
-                    AWAIR_TEMP.set(sensor['value'])
+                    AWAIR_TEMP.labels(device_name).set(sensor['value'])
                 if sensor['comp'] == 'humid':
-                    AWAIR_HUMID.set(sensor['value'])
+                    AWAIR_HUMID.labels(device_name).set(sensor['value'])
                 if sensor['comp'] == 'co2':
-                    AWAIR_CO2.set(sensor['value'])
+                    AWAIR_CO2.labels(device_name).set(sensor['value'])
                 if sensor['comp'] == 'voc':
-                    AWAIR_VOC.set(sensor['value'])
+                    AWAIR_VOC.labels(device_name).set(sensor['value'])
                 if sensor['comp'] == 'pm25':
-                    AWAIR_PM25.set(sensor['value'])     
+                    AWAIR_PM25.labels(device_name).set(sensor['value'])     
     
     except Exception as e:
         print("Failed to retrieve data.")
