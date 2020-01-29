4611f7cab2e0c582c9622638c3badcda13565949
cajaks2 <cajaks2@gmail.com>
2020-01-29 01:25:47 -0800

    clean up variables

--- awair.py
+++ awair.py
@@ -10,47 +10,33 @@ import traceback
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 RESPONSE_CODE = Counter('awair_reponse_code', 'HTTP Response Codes', ['http_code'])
 FAILURE_COUNT = Counter('awair_failure_count', 'AWAIR API FAILURES', ['method'])
-#initialize as none to avoid being 0 
-AWAIR_SCORE = None
-AWAIR_TEMP = None
-AWAIR_HUMID = None
-AWAIR_CO2 = None
-AWAIR_VOC = None
-AWAIR_PM25 = None
+AWAIR_SCORE = Gauge("awair_device_score", "Awair score of device",['device'])
+AWAIR_TEMP = Gauge("awair_device_temp", "Awair temp of device",['device'])
+AWAIR_HUMID = Gauge("awair_device_humid", "Awair humidity of device",['device'])
+AWAIR_CO2 = Gauge("awair_device_co2", "Awair co2 level of device",['device'])
+AWAIR_VOC = Gauge("awair_device_voc", "Awair voc of device",['device'])
+AWAIR_PM25 = Gauge("awair_device_pm25", "Awair pm25 of device",['device'])
+
 
 
 @REQUEST_TIME.time()
 def retrieve_data(auth="",device_name="Bedroom"):
     data = ""
     try:
-        global AWAIR_SCORE
-        global AWAIR_TEMP
-        global AWAIR_HUMID
-        global AWAIR_CO2
-        global AWAIR_PM25
-        global AWAIR_VOC
         data = (get_current_air_data(auth, device_name=device_name))
-        if AWAIR_SCORE is None:
-            AWAIR_SCORE = Gauge("awair_device_score", "Awair score of device",['device'])
-            AWAIR_TEMP = Gauge("awair_device_temp", "Awair temp of device",['device'])
-            AWAIR_HUMID = Gauge("awair_device_humid", "Awair humidity of device",['device'])
-            AWAIR_CO2 = Gauge("awair_device_co2", "Awair co2 level of device",['device'])
-            AWAIR_VOC = Gauge("awair_device_voc", "Awair voc of device",['device'])
-            AWAIR_PM25 = Gauge("awair_device_pm25", "Awair pm25 of device",['device'])
-        string_device = str(device_name)
         for device in data:
-            AWAIR_SCORE.labels(string_device).set(device['score'])
+            AWAIR_SCORE.labels(device_name).set(device['score'])
             for sensor in device['sensors']:
                 if sensor['comp'] == 'temp':
-                    AWAIR_TEMP.labels(string_device).set(sensor['value'])
+                    AWAIR_TEMP.labels(device_name).set(sensor['value'])
                 if sensor['comp'] == 'humid':
-                    AWAIR_HUMID.labels(string_device).set(sensor['value'])
+                    AWAIR_HUMID.labels(device_name).set(sensor['value'])
                 if sensor['comp'] == 'co2':
-                    AWAIR_CO2.labels(string_device).set(sensor['value'])
+                    AWAIR_CO2.labels(device_name).set(sensor['value'])
                 if sensor['comp'] == 'voc':
-                    AWAIR_VOC.labels(string_device).set(sensor['value'])
+                    AWAIR_VOC.labels(device_name).set(sensor['value'])
                 if sensor['comp'] == 'pm25':
-                    AWAIR_PM25.labels(string_device).set(sensor['value'])     
+                    AWAIR_PM25.labels(device_name).set(sensor['value'])     
     
     except Exception as e:
         print("Failed to retrieve data.")
